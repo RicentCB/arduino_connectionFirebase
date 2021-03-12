4d7cbbfb023c12e9e5062deb0bc3ba6b4b0a96fd
RicentCB <ricardo.cb009@gmail.com>
2021-03-12 14:17:42 -0600

    [ADD] Enviar datos de variables

--- emisor.py
+++ emisor.py
@@ -6,16 +6,17 @@ serverDb = "https://arduinointernet-1697e-default-rtdb.firebaseio.com/"
 #Conexion con las base de datos
 firebaseApp = firebase.FirebaseApplication(serverDb, None)
 #Datos a enviar
+sens1 = 150
 data = {
     "id": "2",
-    "sensor1": "1024",
+    "sensor1": sens1,
     "sensor2": "520"
 }
 
 #Llenar DB por metodo POST
-# insData = firebaseApp.post('/test/postEx',data)
-# print(insData)
+insData = firebaseApp.post('/test/postEx/',data)
+print(insData)
 
 #Leer DB por metodo GET
-readData = firebaseApp.get('/test/postEx', '')
-print(readData)
+# readData = firebaseApp.get('/test/postEx/', '2')
+# print(readData)
