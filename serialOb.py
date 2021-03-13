0ee28a86941f0d8b1a5f8a43544feaa181084bc3
RicentCB <ricardo.cb009@gmail.com>
2021-03-13 14:47:19 -0600

    [ADD] Prototipo para leer mensajes

--- serialOb.py
+++ serialOb.py
@@ -2,34 +2,41 @@ import serial
 import sys
 #Definicion del objeto para utilzar el puerto serie
 class SerialOb():
-    def __init__(self, port, baud = 9600):
+    def __init__(self, comPort, baud = 9600):
         #Inicialziar Objeto Serie
         self.ser = serial.Serial(
-            port=self.port,\
-            baudrate=self.baud,\
-            parity=serial.PARITY_NONE,\
-            stopbits=serial.STOPBITS_ONE,\
-            bytesize=serial.EIGHTBITS,\
-            timeout=0)
+            port=comPort,\
+            baudrate=baud,\
+            # parity=serial.PARITY_NONE,\
+            # stopbits=serial.STOPBITS_ONE,\
+            # bytesize=serial.EIGHTBITS,\
+            # timeout=0
+            )
         #Mensaje de bienvenida 
         print("Connectado: " + self.ser.portstr)
 
     
-    def close():
+    def close(self):
         self.ser.close()
-        
-# count=1
-# strMessage = ""
-# while True:
-#     for car in ser.read():
-#         strMessage += chr(car)
-#         if(chr(car) == '\n'):
-#             # print(strMessage)
-#             arrayPartsMessage = strMessage.split(sep=",")
-#             if(len(arrayPartsMessage)>1):
-#                 print(arrayPartsMessage[0]+','+arrayPartsMessage[1])
-#             # Proceso de Mensaje
-#             strMessage = ""
+    
+    #Funcion para escuchar desde el puerto Serie
+    def getFromSerial(self):
+        strMessage = ""
+        while True:
+            for car in self.ser.read():
+                strMessage += chr(car)
+                if(chr(car) == '\n'):
+                    # print(strMessage)
+                    arrayPartsMessage = strMessage.split(sep=",")
+                    if(len(arrayPartsMessage)>1):
+                        print('S1:'+arrayPartsMessage[0]+' S2:'+arrayPartsMessage[1]+' S3:'+arrayPartsMessage[2])
+                    # Proceso de Mensaje
+                    strMessage = ""
 
-def __main__():
-    print ("hello")
+#--------------------------------
+#Termina OBJETO SERIAL
+
+arduino = SerialOb("COM5", 115200)
+#Leer puerto serie del arduino
+
+arduino.close()
